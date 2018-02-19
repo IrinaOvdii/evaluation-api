f63faf1de69ea9877390083022769dd719a7d272
Wouter de Vos <wouter@codaisseur.com>
2018-02-19 11:24:28 +0100

    Fix schema

--- routes/games/players.js
+++ routes/games/players.js
@@ -26,7 +26,7 @@ const getPlayers = (req, res, next) => {
 
         return {
           userId: player.userId,
-          pairs: player.pairs,
+          symbol: player.symbol,
           name
         }
       })
@@ -54,7 +54,7 @@ module.exports = io => {
       }
 
       // Add the user to the players
-      req.game.players = [...req.game.players,{ userId, pairs: [] }]
+      req.game.players = [...req.game.players, { userId }]
 
       req.game.save()
         .then((game) => {
