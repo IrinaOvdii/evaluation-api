0ccb85a8aaf7d8bc4aff2317be630dbb55c5908e
IrynaOvdii <irina.ovdij@gmail.com>
2018-02-22 10:29:47 +0100

    New data

--- models/batch.js
+++ models/batch.js
@@ -2,16 +2,16 @@
 const mongoose = require('../config/database')
 const { Schema } = mongoose
 
-const evaluationsSchema = new Schema({
+const evaluationSchema = new Schema({
   color: { type: String },
   date: { type: Date, default: Date.now },
   remark: { type: String }
 })
 
-const studentsSchema = new Schema({
+const studentSchema = new Schema({
   name: { type: String},
   photo: { type: String },
-  evaluation: [ evaluationsSchema ],
+  evaluation: [ evaluationSchema ],
   lastColor: { type: String }
 })
 
@@ -19,24 +19,8 @@ const batchSchema = new Schema({
   batchNumber: { type: String },
   startDate: { type: Date },
   endDate: { type: Date },
-  students: [ studentsSchema ],
+  students: [ studentSchema ],
   rating:  { type: String }
 })
 
 module.exports = mongoose.model('batches', batchSchema)
-
-// const playerSchema = new Schema({
-//   userId: { type: Schema.Types.ObjectId, ref: 'users' },
-//   symbol: { type: String },
-// });
-//
-// const gameSchema = new Schema({
-//   players: [playerSchema],
-//   winnerId: { type: Schema.Types.ObjectId, ref: 'users' },
-//   userId: { type: Schema.Types.ObjectId, ref: 'users' },
-//   draw: { type: Boolean, default: false },
-//   createdAt: { type: Date, default: Date.now },
-//   updatedAt: { type: Date, default: Date.now },
-// });
-//
-// module.exports = mongoose.model('games', gameSchema)
