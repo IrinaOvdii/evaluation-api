313a9df52d5b1cf414e4544744c7def637e3b0d2
IrynaOvdii <irina.ovdij@gmail.com>
2018-02-24 21:27:10 +0100

    Try to fix add Student

--- routes/batches.js
+++ routes/batches.js
@@ -42,44 +42,44 @@ module.exports = io => {
         })
         .catch((error) => next(error))
     })
-    .patch('/batches/:id', authenticate, (req, res, next) => {
-      const id = req.params.id
-      const userId = req.account._id.toString()
-
-      Batch.findById(id)
-        .then((batch) => {
-          if (!batch) { return next() }
-
-          // const updatedBatch = processMove(batch, req.body, userId)
-
-          Batch.findByIdAndUpdate(id, { $set: updatedBatch }, { new: true })
-            .then((batch) => {
-              io.emit('action', {
-                type: 'BATCH_UPDATED',
-                payload: batch
-              })
-              res.json(batch)
-            })
-            .catch((error) => next(error))
-        })
-        .catch((error) => next(error))
-    })
-    .delete('/batches/:id', authenticate, (req, res, next) => {
-      const id = req.params.id
-      Batch.findByIdAndRemove(id)
-        .then(() => {
-          io.emit('action', {
-            type: 'BATCH_REMOVED',
-            payload: id
-          })
-          res.status = 200
-          res.json({
-            message: 'Removed',
-            _id: id
-          })
-        })
-        .catch((error) => next(error))
-    })
+    // .patch('/batches/:id', authenticate, (req, res, next) => {
+    //   const id = req.params.id
+    //   const userId = req.account._id.toString()
+    //
+    //   Batch.findById(id)
+    //     .then((batch) => {
+    //       if (!batch) { return next() }
+    //
+    //       // const updatedBatch = processMove(batch, req.body, userId)
+    //
+    //       Batch.findByIdAndUpdate(id, { $set: updatedBatch }, { new: true })
+    //         .then((batch) => {
+    //           io.emit('action', {
+    //             type: 'BATCH_UPDATED',
+    //             payload: batch
+    //           })
+    //           res.json(batch)
+    //         })
+    //         .catch((error) => next(error))
+    //     })
+    //     .catch((error) => next(error))
+    // })
+    // .delete('/batches/:id', authenticate, (req, res, next) => {
+    //   const id = req.params.id
+    //   Batch.findByIdAndRemove(id)
+    //     .then(() => {
+    //       io.emit('action', {
+    //         type: 'BATCH_REMOVED',
+    //         payload: id
+    //       })
+    //       res.status = 200
+    //       res.json({
+    //         message: 'Removed',
+    //         _id: id
+    //       })
+    //     })
+    //     .catch((error) => next(error))
+    // })
 
   return router
 }
